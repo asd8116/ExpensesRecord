2435c4012aabde26d68a6478f2beb4d70fa2cb1e
wanaka <asd8116@gmail.com>
2019-04-26 12:45:24 +0800

    Add index action and index view

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const Record = require('../models/record')
 
 router.get('/', (req, res) => {
-  res.send('hello world!')
+  Record.find((err, records) => {
+    if (err) return console.error(err)
+    return res.render('index', { records: records })
+  })
 })
 
 module.exports = router
